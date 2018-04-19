e114862c9120aeb6f5795c503f1394bf0f5ee463
b09027 <kiri@LAPTOP-2BQG5TJT.localdomain>
2018-04-19 20:39:05 +0900

    稼働時に稼働状況を別の WebHook に投入できるように修正

--- src/getmarkets.py
+++ src/getmarkets.py
@@ -3,9 +3,50 @@ import sys
 import urllib.request
 import json
 import slackweb
+import datetime
 
 debug_flag = False
 webhook_url = "https://hooks.slack.com/services/どっかのちゃんねるのWEBHOOK_URL"
+healthcheck_webhook_url = "https://hooks.slack.com/services/どっかのちゃんねるのWEBHOOK_URL"
+
+POST_TYPE_ALERT = 0
+POST_TYPE_HEALTHCHECK = 1
+
+class MarketResult():
+    def __init__(self, result_json = None):
+        self.result_json = result_json
+
+    def parse(self, name = ""):
+        self.result_flag = False
+        self.result_csv = ""
+
+        if self.result_json is None:
+            self.result_api_stat = False
+            self.result_length = 0
+
+        else:
+            self.result_api_stat = True
+            self.result_length = len(self.result_json["result"])
+            self.result_json["result"]
+            tmp_list = []
+
+            for needle in self.result_json["result"]:
+                tmp_list.append(needle["MarketAssetCode"])
+
+                if needle["MarketAssetCode"] == name:
+                    print("found. " + needle["MarketAssetName"])
+                    self.result_flag = True
+
+            tmp_list.sort()
+            for needle in tmp_list:
+                if self.result_csv == "":
+                    self.result_csv = needle
+                else:
+                    self.result_csv = self.result_csv + "," + needle
+
+        if (not self.result_flag):
+           print("not found.")
+
 
 def debug_print(target):
     if debug_flag:
@@ -20,21 +61,43 @@ def getmarket(name):
             debug_print(result)
             result_json = json.loads(result)
 
-        for needle in result_json["result"]:
-            if needle["MarketAssetCode"] == name:
-                print("found. " + needle["MarketAssetName"])
-                return True
-
-        print("not found.")
-        return False
+        result_obj = MarketResult(result_json)
+        result_obj.parse(name)
+        return result_obj
 
     except ValueError :
+        result_obj = MarketResult()
         print("error")
-        return False
+        return returl_obj
 
-def post_to_slack(name):
-    slack = slackweb.Slack(url=webhook_url)
-    slack.notify(text="CE list に " + name + " を発見しました")
+def post_to_slack(post_type, message):
+    if (post_type == POST_TYPE_ALERT):
+        target_url = webhook_url
+    elif (post_type == POST_TYPE_HEALTHCHECK):
+        target_url = healthcheck_webhook_url
+
+    if debug_flag:
+        return
+
+    slack = slackweb.Slack(url=target_url)
+    slack.notify(text=message)
+
+def build_healthcheck_msg(market_result, name):
+    now = datetime.datetime.now()
+    date_str = now.strftime("%Y/%m/%d %H:%M:%S")
+
+    if market_result.result_api_stat:
+        if market_result.result_flag:
+            result_msg = name + " が見つかりました！！"
+        else:
+            result_msg = name + " はまだないみたい（´・ω・｀）"
+
+        msg = "BOT 実行時刻：" + date_str + "\n" + result_msg + "\n\nリスト総数：" + str(market_result.result_length) + "\n現在のリスト：\n```\n" + market_result.result_csv + "\n```"
+
+    else:
+        msg = "BOT 実行時刻： " + date_str + "\nCE API 実行結果を解析できませんでした。"
+
+    return msg
 
 def main():
     argv = sys.argv
@@ -45,8 +108,14 @@ def main():
 
     target_code = argv[1]
     market_result = getmarket(target_code)
-    if market_result:
-        post_to_slack(target_code)
+
+    if market_result.result_flag:
+        post_to_slack(POST_TYPE_ALERT, "CE list に " + target_code + " を発見しました")
+
+    healthcheck_msg = build_healthcheck_msg(market_result, target_code)
+    debug_print(healthcheck_msg)
+    post_to_slack(POST_TYPE_HEALTHCHECK, healthcheck_msg)
+
 
 
 if __name__=='__main__':
