1aa11c16f83ee475c13b48ea3e14728173ab751b
b09027 <kiri@LAPTOP-2BQG5TJT.localdomain>
2018-04-23 03:27:08 +0900

    取引所への新規コインリストアップ、および削除を検知して3つ目のslackチャンネルに通知する機能を追加

--- src/getmarkets.py
+++ src/getmarkets.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
+import os
+import os.path
 import urllib.request
 import json
 import slackweb
@@ -9,20 +11,27 @@ debug_flag = False
 
 webhook_url = "https://hooks.slack.com/services/どっかのちゃんねるのWEBHOOK_URL"
 healthcheck_webhook_url = "https://hooks.slack.com/services/どっかのちゃんねるのWEBHOOK_URL"
+exchange_listup_hook_url = "https://hooks.slack.com/services/どっかのちゃんねるのWEBHOOK_URL"
 
 POST_TYPE_ALERT = 0
 POST_TYPE_HEALTHCHECK = 1
+POST_TYPE_LISTUP = 2
 
 EXCHANGE_TYPE_CE = 0
 
 class ExchangeApiExecutor():
     def __init__(self, search_target_code):
-        self.search_target_code = search_target_code
-        self.result_json = None
-        self.result_flag = False
-        self.result_csv = ""
-        self.result_api_stat = False
-        self.result_length = 0
+        self.search_target_code = search_target_code #調査対象コード
+        self.result_json = None                      #API 実施結果の JSON
+        self.result_api_stat = False                 #API の HTTP 疎通状態
+        self.result_flag = False                     #コード検索結果
+        self.result_code_list = []                   #API 実施結果で取得できたコードリスト
+        self.result_length = 0                       #API 実施結果リストのサイズ
+        self.new_only_list = []                      #前結果比較時に、新結果のみにあるコード
+        self.old_only_list = []                      #前結果比較時に、前結果のみにあるコード
+
+        now = datetime.datetime.now()
+        self.date_str = now.strftime("%Y/%m/%d %H:%M:%S")
 
     def call_api(self):
         return
@@ -31,7 +40,59 @@ class ExchangeApiExecutor():
         self.result_api_stat = False
         self.result_length = 0
         return
+
+    def get_code_csv(self):
+        ret_csv = ""
+        for needle in self.result_code_list:
+            if ret_csv == "":
+                ret_csv = needle
+            else:
+                ret_csv = ret_csv + "," + needle
+        return ret_csv
         
+    def compare_with_last_result(self):
+        if (not self.result_api_stat or self.result_length == 0):
+            print("skip compare_with_last_result()")
+            return
+
+        if (os.path.exists("../conf/last_result.json")):
+            with open("../conf/last_result.json", "r") as file_obj:
+                json_config = json.load(file_obj)
+
+            debug_print("{0}".format(json.dumps(json_config, indent=4)))
+
+            if (self.get_exchange_name() in json_config):
+                for needle in self.result_code_list:
+                    if needle not in json_config[self.get_exchange_name()]["last_result"]:
+                        #前のリストになかった
+                        self.new_only_list.append(needle)
+                        print(needle + " is listed.")
+
+                for needle in json_config[self.get_exchange_name()]["last_result"]:
+                    if needle not in self.result_code_list:
+                        #今回のリストになかった
+                        self.old_only_list.append(needle)
+                        print(needle + " is delisted.")
+
+                json_config[self.get_exchange_name()]["last_result"] = self.result_code_list
+
+            else:
+                my_dict = {"last_timestamp": self.date_str, "last_result": self.result_code_list}
+                json_config[self.get_exchange_name()] = my_dict
+                print(self.get_exchange_name() + " date is created.")
+
+        else:
+            if (not os.path.exists("../conf/")):
+                os.makedirs("../conf/")
+            my_dict = {"last_timestamp": self.date_str, "last_result": self.result_code_list}
+            json_config = {self.get_exchange_name(): my_dict}
+            print("conf file is created.")
+
+
+        with open("../conf/last_result.json", "w") as file_obj:
+            json.dump(json_config, file_obj, indent=4)
+
+
     def get_exchange_name(self):
         return "parent class"
 
@@ -63,23 +124,17 @@ class CoinExchangeExecutor(ExchangeApiExecutor):
 
         else:
             self.result_api_stat = True
-            self.result_length = len(self.result_json["result"])
-            self.result_json["result"]
-            tmp_list = []
 
             for needle in self.result_json["result"]:
-                tmp_list.append(needle["MarketAssetCode"])
+                if (needle["MarketAssetCode"] not in self.result_code_list):
+                    self.result_code_list.append(needle["MarketAssetCode"])
 
-                if self.search_target_code != "" and needle["MarketAssetCode"] == self.search_target_code:
-                    print("found. " + needle["MarketAssetName"])
-                    self.result_flag = True
+                    if self.search_target_code != "" and needle["MarketAssetCode"] == self.search_target_code:
+                        print("found. " + needle["MarketAssetName"])
+                        self.result_flag = True
 
-            tmp_list.sort()
-            for needle in tmp_list:
-                if self.result_csv == "":
-                    self.result_csv = needle
-                else:
-                    self.result_csv = self.result_csv + "," + needle
+            self.result_code_list.sort()
+            self.result_length = len(self.result_code_list)
 
         if (self.search_target_code != "" and not self.result_flag):
            print("not found.")
@@ -97,6 +152,8 @@ def post_to_slack(post_type, message):
         target_url = webhook_url
     elif (post_type == POST_TYPE_HEALTHCHECK):
         target_url = healthcheck_webhook_url
+    elif (post_type == POST_TYPE_LISTUP):
+        target_url = exchange_listup_hook_url
 
     if debug_flag:
         return
@@ -105,9 +162,6 @@ def post_to_slack(post_type, message):
     slack.notify(text=message)
 
 def build_healthcheck_msg(exchange_obj, search_target_code):
-    now = datetime.datetime.now()
-    date_str = now.strftime("%Y/%m/%d %H:%M:%S")
-
     if exchange_obj.result_api_stat:
         if search_target_code == "":
             result_msg = "コイン名称検索は実施せず、リスト取得のみを行いました。"
@@ -116,10 +170,10 @@ def build_healthcheck_msg(exchange_obj, search_target_code):
         else:
             result_msg = search_target_code + " はまだないみたい（´・ω・｀）"
 
-        msg = "*【" + exchange_obj.get_exchange_name() + "】*\nBOT 実行時刻：" + date_str + "\n" + result_msg + "\n\nリスト総数：" + str(exchange_obj.result_length) + "\n現在の " + exchange_obj.get_exchange_name() + " リスト：\n```\n" + exchange_obj.result_csv + "\n```"
+        msg = "*【" + exchange_obj.get_exchange_name() + "】*\nBOT 実行時刻：" + exchange_obj.date_str + "\n" + result_msg + "\n\nリスト総数：" + str(exchange_obj.result_length) + "\n現在の " + exchange_obj.get_exchange_name() + " リスト：\n```\n" + exchange_obj.get_code_csv() + "\n```"
 
     else:
-        msg = "*【" + exchange_obj.get_exchange_name() + "】*\nBOT 実行時刻： " + date_str + "\nAPI 実行結果を解析できませんでした。"
+        msg = "*【" + exchange_obj.get_exchange_name() + "】*\nBOT 実行時刻： " + exchange_obj.date_str + "\nAPI 実行結果を解析できませんでした。"
 
     return msg
 
@@ -139,9 +193,17 @@ def main():
 
     exchange_obj = CoinExchangeExecutor(target_code)
     exchange_obj.call_api()
+    exchange_obj.compare_with_last_result()
 
     if exchange_obj.result_flag:
         post_to_slack(POST_TYPE_ALERT, exchange_obj.get_exchange_name() + " list に " + target_code + " を発見しました")
+
+    if len(exchange_obj.new_only_list) != 0:
+        post_to_slack(POST_TYPE_LISTUP, exchange_obj.get_exchange_name() + " list に " + str(exchange_obj.new_only_list) + " がリストアップされました！")
+
+    if len(exchange_obj.old_only_list) != 0:
+        post_to_slack(POST_TYPE_LISTUP, exchange_obj.get_exchange_name() + " list から " + str(exchange_obj.old_only_list) + " が削除されました...")
+
 
     healthcheck_msg = build_healthcheck_msg(exchange_obj, target_code)
     debug_print(healthcheck_msg)
