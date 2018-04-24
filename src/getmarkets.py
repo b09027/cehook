8b5054917c37372bd4cbe248b673b184ae0fdd70
b09027 <kiri@LAPTOP-2BQG5TJT.localdomain>
2018-04-25 03:08:04 +0900

    Binance 対応を実施

--- src/getmarkets.py
+++ src/getmarkets.py
@@ -29,6 +29,7 @@ class ExchangeApiExecutor():
         self.result_length = 0                       #API 実施結果リストのサイズ
         self.new_only_list = []                      #前結果比較時に、新結果のみにあるコード
         self.old_only_list = []                      #前結果比較時に、前結果のみにあるコード
+        self.additional_info = None                  #追加で出力したい内容があるなら文字列を設定する
 
         now = datetime.datetime.now()
         self.date_str = now.strftime("%Y/%m/%d %H:%M:%S")
@@ -49,7 +50,7 @@ class ExchangeApiExecutor():
             else:
                 ret_csv = ret_csv + "," + needle
         return ret_csv
-        
+
     def compare_with_last_result(self):
         if (not self.result_api_stat or self.result_length == 0):
             print("skip compare_with_last_result()")
@@ -79,7 +80,7 @@ class ExchangeApiExecutor():
             else:
                 my_dict = {"last_timestamp": self.date_str, "last_result": self.result_code_list}
                 json_config[self.get_exchange_name()] = my_dict
-                print(self.get_exchange_name() + " date is created.")
+                print(self.get_exchange_name() + " data is created.")
 
         else:
             if (not os.path.exists("../conf/")):
@@ -143,6 +144,69 @@ class CoinExchangeExecutor(ExchangeApiExecutor):
         return "CoinExchange"
 
 
+class BinanceExecutor(ExchangeApiExecutor):
+    def __init__(self, search_target_code):
+        super().__init__(search_target_code)
+
+    def call_api(self):
+        url = "https://www.binance.com/api/v1/ticker/24hr"
+
+        try :
+            with urllib.request.urlopen(url) as res:
+                result = res.read().decode("utf-8")
+                debug_print(result)
+                self.result_json = json.loads(result)
+
+            self.parse()
+            return
+
+        except ValueError :
+            print("error")
+            return
+
+    def parse(self):
+        price_change_percent_list = []
+
+        if self.result_json is None:
+            self.result_api_stat = False
+            self.result_length = 0
+
+        else:
+            self.result_api_stat = True
+
+            for needle in self.result_json:
+                if (needle["symbol"] not in self.result_code_list):
+                    self.result_code_list.append(needle["symbol"])
+                    price_change_percent_list.append({"name":needle["symbol"], "percent":float(needle["priceChangePercent"]), "volume":needle["volume"]})
+                    
+
+                    if self.search_target_code != "" and needle["symbol"] == self.search_target_code:
+                        print("found. " + needle["symbol"])
+                        self.result_flag = True
+
+            self.result_code_list.sort()
+            self.result_length = len(self.result_code_list)
+
+            sorted_percent_list = sorted(price_change_percent_list, key=lambda x: x["percent"], reverse=True)
+            if (len(sorted_percent_list) >= 20):
+                # 20銘柄以下はソートした価格変化率を表示しない
+                self.additional_info = "\n```\n騰落率 上位下位 10ペア\n\n"
+
+                for i in range(0,9):
+                    self.additional_info += sorted_percent_list[i]["name"] + ": " + str(sorted_percent_list[i]["percent"]) + "% (volume: " + sorted_percent_list[i]["volume"] + ")\n"
+
+                self.additional_info += "\n" 
+                for i in range(len(sorted_percent_list) - 10, len(sorted_percent_list) - 1):
+                    self.additional_info += sorted_percent_list[i]["name"] + ": " + str(sorted_percent_list[i]["percent"]) + "% (volume: " + sorted_percent_list[i]["volume"] + ")\n"
+                self.additional_info += "```"
+
+        if (self.search_target_code != "" and not self.result_flag):
+           print("not found.")
+
+    def get_exchange_name(self):
+        return "Binance"
+
+
 def debug_print(target):
     if debug_flag:
         print(target)
@@ -172,6 +236,9 @@ def build_healthcheck_msg(exchange_obj, search_target_code):
 
         msg = "*【" + exchange_obj.get_exchange_name() + "】*\nBOT 実行時刻：" + exchange_obj.date_str + "\n" + result_msg + "\n\nリスト総数：" + str(exchange_obj.result_length) + "\n現在の " + exchange_obj.get_exchange_name() + " リスト：\n```\n" + exchange_obj.get_code_csv() + "\n```"
 
+        if (exchange_obj.additional_info is not None):
+            msg = msg + "\n" + exchange_obj.additional_info
+
     else:
         msg = "*【" + exchange_obj.get_exchange_name() + "】*\nBOT 実行時刻： " + exchange_obj.date_str + "\nAPI 実行結果を解析できませんでした。"
 
@@ -181,7 +248,7 @@ def main():
     argv = sys.argv
     argc = len(argv)
     if (argc != 2 and argc != 3):
-        print("Usage: python " + argv[0] + " EXCHANGE_NAME [TARGET_CODE]\nEXCHANGE_NAME: CE")
+        print("Usage: python " + argv[0] + " EXCHANGE_NAME [TARGET_CODE]\nEXCHANGE_NAME: CE or Binance")
         quit()
 
     exchange_name = argv[1]
@@ -191,7 +258,11 @@ def main():
     elif (argc == 3):
         target_code = argv[2]
 
-    exchange_obj = CoinExchangeExecutor(target_code)
+    if (exchange_name == "CE"):
+        exchange_obj = CoinExchangeExecutor(target_code)
+    elif (exchange_name == "Binance"):
+        exchange_obj = BinanceExecutor(target_code)
+
     exchange_obj.call_api()
     exchange_obj.compare_with_last_result()
 
